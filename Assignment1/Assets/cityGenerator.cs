305ec468a87118cc5cfe8398fc1a06241ac74564
Arek Rybicki <zysio1998@gmail.com>
2020-12-22 19:27:30 +0000

    making logic to randomly show buildings

---
+++ Assignment1/Assets/cityGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cityGenerator : MonoBehaviour
+{
+    public GameObject[] buildings;
+    public int mapWidth = 20;
+    public int mapHeight = 20;
+    int buildingSpreader = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int h = 0; h < mapHeight; h++)
+        {
+            for (int w = 0; w < mapWidth; w++)
+            {
+                Vector3 pos = new Vector3(w * buildingSpreader, 0, h * buildingSpreader);
+                int n = Random.Range(0, buildings.Length);
+                Instantiate(buildings[n], pos, Quaternion.identity);
+
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
