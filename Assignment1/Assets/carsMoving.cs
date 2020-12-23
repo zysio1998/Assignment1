b06b092f94a0e007cf6741bdc8acb82c6c0ed0c8
Arek Rybicki <zysio1998@gmail.com>
2020-12-23 14:33:47 +0000

    cars moving from both sides

--- Assignment1/Assets/carsMoving.cs
+++ Assignment1/Assets/carsMoving.cs
@@ -13,6 +13,6 @@ public class carsMoving : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.forward * Time.deltaTime;        
+        transform.position -= transform.forward * Time.deltaTime;        
     }
 }
